dc1ead303590fb88ef40c74f405c906278da9104
shahidafridi65 <86219782+shahidafridi65@users.noreply.github.com>
2022-12-25 17:42:14 +0530

    Friends changes

--- app.py
+++ app.py
@@ -7,9 +7,34 @@ def hello_world():
 
 @app.route('/api/friends')
 def frients_list():
-    return [
-        'Adil',
-        'Afrid',
-        'Asif',
-        'Shoyab'
-    ]
+    return Adil = {
+    "name": "Adil Khan",
+    "email": "adil@gamil.com",
+    "phone": "067765665656",
+    "address": "Guntur",
+    "country": "India"
+}
+
+Asif = {
+    "name": "Asif Shaik",
+    "email": "asifshaik@gamil.com",
+    "phone": "09876546587",
+    "address": "Triupathi",
+    "country": "India"
+}
+
+Shoyab = {
+    "name": "Shoyab Shaik",
+    "email": "Shoyabsk@gamil.com",
+    "phone": "067765665656",
+    "address": "Guntur",
+    "country": "India"
+}
+
+Aslam = {
+    "name": "Aslam Shaik",
+    "email": "aslamshaik@gamil.com",
+    "phone": "09878766676",
+    "address": "Triupathi",
+    "country": "India"
+}
